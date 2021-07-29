2a3a41efb148ddbe875e6f05e6cb4a88b43201fb
Gabriel Costa <ribeirocosta.rj1@gmail.com>
2021-07-29 16:48:06 +0100

    Removed the service calls from MoviesMenuActivity.kt and moved to specific class MovieRepository.kt

--- app/src/main/java/com/example/mobiletoyou/activities/MoviesMenuActivity.kt
+++ app/src/main/java/com/example/mobiletoyou/activities/MoviesMenuActivity.kt
@@ -3,22 +3,20 @@ package com.example.mobiletoyou.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobiletoyou.*
 import com.example.mobiletoyou.adapters.MenuMoviesAdapter
 import com.example.mobiletoyou.model.Movie
-import com.example.mobiletoyou.network.MoviesListResponse
-import com.example.mobiletoyou.network.RetrofitInitializer
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.mobiletoyou.network.MovieRepository
 
 class MoviesMenuActivity : AppCompatActivity() {
     private val movie: MutableList<Movie> = mutableListOf()
     private lateinit var gridLayoutManager: GridLayoutManager
+    private val repository: MovieRepository by lazy {
+        MovieRepository()
+    }
     private val menuMoviesAdapter: MenuMoviesAdapter by lazy {
         MenuMoviesAdapter(this, movie = movie, getMovieItemClickListener())
     }
@@ -31,26 +29,17 @@ class MoviesMenuActivity : AppCompatActivity() {
         gridLayoutManager = GridLayoutManager(this, 3, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = gridLayoutManager
 
-        getMoviesList(menuMoviesAdapter)
+        getMoviesList()
     }
 
-    private fun getMoviesList(menuMoviesAdapter: MenuMoviesAdapter) {
-        RetrofitInitializer().service.getPopularMovies(apiKey = API_KEY)
-            .enqueue(object : Callback<MoviesListResponse>{
-                override fun onResponse(
-                    call: Call<MoviesListResponse>,
-                    response: Response<MoviesListResponse>
-                ) {
-                    val moviesListResponse = response.body()?.movieList
-                    if (moviesListResponse != null) {
-                        menuMoviesAdapter.setData(moviesListResponse)
-                    }
+    private fun getMoviesList() {
+        repository.getMoviesList(object : MovieRepository.OnMoviesListSuccess {
+            override fun onMoviesListResponseSuccess(movieList: MutableList<Movie>?) {
+                if (movieList != null) {
+                    menuMoviesAdapter.setData(movies = movieList)
                 }
-
-                override fun onFailure(call: Call<MoviesListResponse>, t: Throwable) {
-                    Toast.makeText(this@MoviesMenuActivity, "$ERROR_MSG", Toast.LENGTH_SHORT).show()
-                }
-            })
+            }
+        })
     }
 
     private fun getMovieItemClickListener(): MenuMoviesAdapter.MovieItemClickListener {
--- app/src/main/java/com/example/mobiletoyou/network/MovieRepository.kt
+++ app/src/main/java/com/example/mobiletoyou/network/MovieRepository.kt
@@ -1,10 +1,6 @@
 package com.example.mobiletoyou.network
 
-import android.app.Application
-import android.widget.Toast
-import com.example.mobiletoyou.API_KEY
-import com.example.mobiletoyou.ERROR_MSG
-import com.example.mobiletoyou.adapters.MovieSuggestionsAdapter
+import com.example.mobiletoyou.model.Movie
 import com.example.mobiletoyou.model.MovieDetails
 import com.example.mobiletoyou.model.SuggestedMovie
 import retrofit2.Call
@@ -41,9 +37,30 @@ class MovieRepository {
                 }
 
                 override fun onFailure(call: Call<MoviesSuggestionsResponse>, t: Throwable) {
-                    //Toast.makeText(this@MainActivity, ERROR_MSG, Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(Application.context, ERROR_MSG, Toast.LENGTH_SHORT).show()
                 }
             })
+    }
+
+    fun getMoviesList(success: OnMoviesListSuccess) {
+        RetrofitInitializer().service.getPopularMovies()
+            .enqueue(object : Callback<MoviesListResponse>{
+                override fun onResponse(
+                    call: Call<MoviesListResponse>,
+                    response: Response<MoviesListResponse>
+                ) {
+                    val moviesListResponse = response.body()?.movieList
+                    success.onMoviesListResponseSuccess(movieList = moviesListResponse)
+                }
+
+                override fun onFailure(call: Call<MoviesListResponse>, t: Throwable) {
+                    //Toast.makeText(this@MoviesMenuActivity, "$ERROR_MSG", Toast.LENGTH_SHORT).show()
+                }
+            })
+    }
+
+    interface OnMoviesListSuccess {
+        fun onMoviesListResponseSuccess(movieList: MutableList<Movie>?)
     }
 
     interface OnMovieSuccess {
