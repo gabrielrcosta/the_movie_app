4f862440511415de457115f4fef13139c52b3631
Gabriel Costa <ribeirocosta.rj1@gmail.com>
2021-08-05 20:02:23 +0100

    Added cast list to movie information and suggested movies

--- app/src/main/java/com/example/mobiletoyou/activities/MainActivity.kt
+++ app/src/main/java/com/example/mobiletoyou/activities/MainActivity.kt
@@ -2,43 +2,86 @@ package com.example.mobiletoyou.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobiletoyou.*
+import com.example.mobiletoyou.adapters.CastAdapter
 import com.example.mobiletoyou.adapters.MovieSuggestionsAdapter
+import com.example.mobiletoyou.databinding.ActivityMainBinding
+import com.example.mobiletoyou.model.Cast
 import com.example.mobiletoyou.model.MovieDetails
 import com.example.mobiletoyou.model.SuggestedMovie
 import com.example.mobiletoyou.network.MovieRepository
-
+import com.example.mobiletoyou.utilities.ItemClickListener
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private val moviesList: MutableList<SuggestedMovie> = mutableListOf()
+    private val castList: MutableList<Cast> = mutableListOf()
     private val repository: MovieRepository by lazy {
         MovieRepository()
     }
     private val moviesSuggestionsAdapter: MovieSuggestionsAdapter by lazy {
         MovieSuggestionsAdapter(this, moviesList, getMovieItemClickListener())
     }
+    private val castAdapter = CastAdapter(this, castList, getCastItemClickListener())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-        recyclerView.adapter = moviesSuggestionsAdapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.castRecyclerView.adapter = castAdapter
+        binding.castRecyclerView.layoutManager =
+            LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+
+        binding.recyclerView.adapter = moviesSuggestionsAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
         val movieId = intent.getIntExtra(NEXT_MOVIE, MOVIE_ID)
 
-        getMovieDetails(movieId)
-        getSuggestedMovies(movieId)
+        getMovieDetails(movieId = movieId)
+        getCastList(movieId = movieId)
+        getSuggestedMovies(movieId = movieId)
     }
 
     private fun getMovieDetails(movieId: Int) {
         repository.getMovieDetails(movieId = movieId, object: MovieRepository.OnMovieSuccess{
             override fun onMovieDetailsSuccess(movieDetails: MovieDetails?) {
-                moviesSuggestionsAdapter.setMovieDetails(movieDetails)
+                binding.apply {
+                    layoutIncluded.apply {
+                        Picasso.get().load(MOVIE_URL + movieDetails?.posterPath).into(backgroundPicture)
+                        likeMovieClick.visibility = View.INVISIBLE
+                        likes.text = movieDetails?.voteCount.toString()
+                        title.text = movieDetails?.title
+                        overview.text = movieDetails?.overview
+                        viewsText.text = movieDetails?.popularity.toString()
+                        likeMovie.setOnClickListener {
+                            likeMovie.visibility =
+                                View.INVISIBLE; likeMovieClick.visibility = View.VISIBLE; likes.text =
+                            movieDetails?.voteCount?.plus(1).toString()
+                        }
+                        likeMovieClick.setOnClickListener {
+                            likeMovieClick.visibility =
+                                View.INVISIBLE; likeMovie.visibility =
+                            View.VISIBLE; likes.text =
+                            movieDetails?.voteCount.toString()
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    private fun getCastList(movieId: Int) {
+        repository.getCastList(movieId = movieId, object : MovieRepository.OnMovieCastSuccess {
+            override fun onMovieCastResponseSuccess(cast: MutableList<Cast>) {
+                castAdapter.setCastData(cast)
             }
         })
     }
@@ -57,9 +100,20 @@ class MainActivity : AppCompatActivity() {
         return object :
             MovieSuggestionsAdapter.MovieItemClickListener {
             override fun onItemMovieClicked(id: Int) {
-                val intent = Intent(this@MainActivity, CastMovieActivity::class.java)
+                val intent = Intent(this@MainActivity, MainActivity::class.java)
                 intent.putExtra(NEXT_MOVIE, id)
                 startActivity(intent)
+            }
+        }
+    }
+
+    private fun getCastItemClickListener(): ItemClickListener {
+        return object : ItemClickListener {
+            override fun onItemViewClicked(id: Int) {
+                val intent = Intent(this@MainActivity, CastInformationActivity::class.java)
+                intent.putExtra(GET_PERSONAL_ID, id)
+                startActivity(intent)
+                Log.e("Error", "$id")
             }
         }
     }
